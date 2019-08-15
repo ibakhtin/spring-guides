fad175fdc5a555bf0a0f4968220ffbfd76dc1abe
Igor Bakhtin <ibakhtin@icloud.com>
2019-08-15 19:01:53 +0300

    Done Accessing Data with MongoDB

---
+++ gs-accessing-data-mongodb/initial/src/main/java/hello/Customer.java
@@ -0,0 +1,23 @@
+package hello;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+
+    @Id
+    private String id;
+
+    private String firstName;
+    private String lastName;
+
+    public Customer(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
